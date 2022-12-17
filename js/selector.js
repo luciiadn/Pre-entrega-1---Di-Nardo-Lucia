bd78637ab3497af0ca0536e4824dfa16de2d949e
Lucia Di Nardo <luciiadn@gmail.com>
2022-12-17 11:12:44 -0300

    push

--- js/selector.js
+++ js/selector.js
@@ -1,8 +1,9 @@
 const contenedor = document.querySelector('#contenedorTarjetas');
 const container = document.querySelector('#cardContainer');
-const selectProfesion = document.querySelector('#profesion');
-const btnBuscar = document.querySelector('#buscar');
-const searchBtn = document.querySelector('#search');
+const selectProfesion = document.getElementById('#profesion');
+const btnBuscar = document.querySelector('#btnBuscar');
+const buscar = document.getElementById ('buscar')
+
 
 function filtrarProfesion(array){
     let profesion = selectProfesion.value;
@@ -17,28 +18,29 @@ function filtrarProfesion(array){
 function createHTML(array) {
     contenedor.innerHTML = '';
     container.innerHTML = '';
-    array.forEach ((nombre) => {
+    array.forEach ((especialidad) => {
         const tarjeta = `
         <div class="col">
                 <div class="card h-100">
-                     <img src="${nombre.imagen}" class="card-img-top" alt="${nombre.especialidad}">
+                     <img src="${especialidad.imagen}" class="card-img-top" alt="${especialidad.especialidad}">
                      <div class="card-body">
-                        <h4 class="card-tittle">${nombre.especialidad}</h4>
-                        <p class="card-text">Nombre: ${nombre.nombre}</p>
+                        <h4 class="card-tittle">${especialidad.especialidad}</h4>
+                        <p class="card-text">Nombre: ${especialidad.nombre}</p>
                     </div>
                 </div>
         </div>`;
                 contenedor.innerHTML += tarjeta;              
         
     })
+    
+      
+    buscar.addEventListener('click', () => {
+        fetch("/js/data.json")
+            .then(response => response.json())
+            .then (data => {
+                localStorage.setItem('user',JSON.stringify(data));
+                createHTML(filtrarProfesion(data));
+            })
+    })
 }
-
-btnBuscar.addEventListener('click', () => {
-    fetch('js/data.json')
-        .then((response) => response.json())
-        .then ((data) => {
-            localStorage.setItem('user',JSON.stringify(data));
-            createHTML(filtrarProfesion(data));
-        })
-})
 
